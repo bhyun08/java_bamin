3a38d1f7b9dabba38125879517e8d12cedb4bd8f
나병현 <nbhyun0329@gmail.com>
2024-04-19 08:17:29 +0900

    연산자 문제 풀기

---
+++ OperationEx3.java
@@ -0,0 +1,10 @@
+package opertator_2.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+
+        int score = 80;
+        boolean sscore = 80 <= score && score <= 100;
+        System.out.println(sscore);
+    }
+}
