3197897a5243eaba80fc6e530c894c1aaba5703e
나병현 <nbhyun0329@gmail.com>
2024-04-22 08:10:37 +0900

    반복문

---
+++ 4.loop/ex/WhileEx1.java
@@ -0,0 +1,12 @@
+package loop_4.ex;
+
+public class WhileEx1 {
+
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 10) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
