034e00d2d43c64357c066e92a1d8513d7cd16306
나병현 <nbhyun0329@gmail.com>
2024-04-26 08:25:28 +0900

    스캐너

---
+++ 7.scanner/ex/ChangeVarEx.java
@@ -0,0 +1,13 @@
+package scanner_7.ex;
+
+public class ChangeVarEx {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp;
+        //시작: 코드를 작성하세요
+        //종료: 코드를 작성하세요
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
